15f7a5d348c575dccf0bec35280855ed5c07c36a
Zelenskih Mark <z.mark366@gmail.com>
2021-05-07 18:08:02 +0700

    Add files via upload

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -1,11 +1,75 @@
 import React, {Component} from "react";
+import {Col, Container, Nav, Row, Tab} from "react-bootstrap";
+import {Button} from "bootstrap";
 
 export default class Docs extends Component{
     render() {
         return (
-            <div className="m-5">
-                Hello Docs
-            </div>
+            <Container>
+                <Tab.Container id="ledt-tabs-example" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column mt-3">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">S</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">T</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">A</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fourth">S</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fifth">S</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content className="mt-3">
+                                <Tab.Pane eventKey="first">
+                                    <img src="https://images.wallpaperscraft.ru/image/siluet_luna_lodka_135277_225x300.jpg"/>
+                                    <p>
+                                        Информация которую нужно заменить,
+                                        но мне оооочень лень.
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <img src="https://images.wallpaperscraft.ru/image/siluet_luna_lodka_135277_225x300.jpg"/>
+                                    <p>
+                                        Информация которую нужно заменить,
+                                        но мне оооочень лень.
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <img src="https://image.winudf.com/v2/image1/Y29tLmhkd2FsbHBhcGVyLndhbGxwYXBlcl9zY3JlZW5fcnUtUlVfNl8xNjAzMTE3MTYzXzAxMA/screen-6.jpg?fakeurl=1&type=.jpg"/>
+                                    <p>
+                                        Информация которую нужно заменить,
+                                        но мне оооочень лень.
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fourth">
+                                    <img src="https://image.winudf.com/v2/image1/Y29tLmhkd2FsbHBhcGVyLndhbGxwYXBlcl9zY3JlZW5fcnUtUlVfNl8xNjAzMTE3MTYzXzAxMA/screen-6.jpg?fakeurl=1&type=.jpg"/>
+                                    <p>
+                                        Информация которую нужно заменить,
+                                        но мне оооочень лень.
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fifth">
+                                    <img src="https://image.winudf.com/v2/image1/Y29tLmhkd2FsbHBhcGVyLndhbGxwYXBlcl9zY3JlZW5fcnUtUlVfNl8xNjAzMTE3MTYzXzAxMA/screen-6.jpg?fakeurl=1&type=.jpg"/>
+                                    <p>
+                                        Информация которую нужно заменить,
+                                        но мне оооочень лень.
+                                    </p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+
+            </Container>
         )
     }
 }
--- src/pages/Sensors.js
+++ src/pages/Sensors.js
@@ -1,11 +1,40 @@
 import React, {Component} from "react";
+import {Container, Form, Button} from "react-bootstrap";
 
 export default class Sensors extends Component{
     render() {
         return (
-            <div className="m-5">
-                Hello Sensors
-            </div>
+            <Container style={{width: '500px'}}>
+                <h1 className="text-center">Введите данные</h1>
+                <Form>
+                    <Form.Group controlId="formBasicStartDate">
+                        <Form.Label>Дата начала</Form.Label>
+                        <Form.Control type="date" placeholder="Введите дату"/>
+                        <Form.Text>
+                            BLablablabla
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEndDate">
+                        <Form.Label>Дата конца</Form.Label>
+                        <Form.Control type="time" placeholder="Введите время"/>
+                        <Form.Text>
+                            BLablablabla
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicValue">
+                        <Form.Label>Введите значение</Form.Label>
+                        <Form.Control as="textarea" rows="1"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Отправить</Button>
+                </Form>
+            </Container>
+
         )
     }
 }
